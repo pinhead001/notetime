3c9bdc6c35d289a58c80acd1e1e2be9d547c6b1e
Chris Nelson <pinhead001@gmail.com>
2026-01-28 23:58:34 -0700

    prepped for render

--- notetime/db.py
+++ notetime/db.py
@@ -1,14 +1,24 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, MappedAsDataclass
+from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, sessionmaker
+
+from notetime.config import settings
 
 # SQLite database path
-DATABASE_URL = "sqlite:///notetime.db"
+# DATABASE_URL = "sqlite:///notetime.db"
+
+# Handle SQLite-specific connect_args
+connect_args = {}
+if "sqlite" in settings.DATABASE_URL:
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(settings.DATABASE_URL, connect_args=connect_args)
 
 # Engine
 engine = create_engine(DATABASE_URL, echo=False)
 
 # Session factory
 SessionLocal = sessionmaker(bind=engine)
+
 
 # Base class for ORM models
 class Base(MappedAsDataclass, DeclarativeBase):
